6c8c4453a9bc1076db67f1c00df746a99cb0836f
Fyryte <131640588+Fyryte@users.noreply.github.com>
2023-04-25 21:25:31 -0600

    Update KSPC_Experimental.ino

--- KSPC_Experimental.ino
+++ KSPC_Experimental.ino
@@ -1,3 +1,45 @@
+#include <LiquidCrystal_I2C.h>        // LCD Display
+#include <KerbalSimpit.h>             // SimPit     
+#include <KerbalSimpitMessageTypes.h> // SimPit
+#include <PayloadStructs.h>           // SimPit
+
+
+KerbalSimpit mySimpit(Serial);
+LiquidCrystal_I2C lcd(0x3F,16,2);
+
+char apoapsis_str[150];  // telemetry strings
+char periapsis_str[150];
+
+void setup() {
+    while (!mySimpit.init()) {         
+    delay(100);                         // Handshake protocol attempts ever 100ms until connection   
+     Serial.begin(115200);              // Initializes the serial communication
+  lcd.init();                           // LCD Display for the rest until the end of bracket
+  lcd.backlight();
+  lcd.print("   Working");
+  delay(500);
+  lcd.print(".");
+  delay(500);
+  lcd.print(".");
+  delay(500);
+  lcd.print(".");
+  delay(500);
+
+
+  }
+  lcd.clear();                          // Once handshake has been completed on game launch this block prints connected to SimPit in game and the LCD
+  lcd.print("   Connected!");
+  mySimpit.printToKSP("Connected", PRINT_TO_SCREEN);
+
+  mySimpit.registerChannel(APSIDES_MESSAGE); // register input channel
+  mySimpit.inboundHandler(messageHandler);   // register message handler
+}
+
+  void loop() {
+    lcd.backlight();
+    mySimpit.update(); //processes incoming messages
+}
+
 void messageHandler(byte messageType, byte msg[], byte msgSize) {
   switch (messageType) {
     case APSIDES_MESSAGE:
@@ -33,7 +75,7 @@ void messageHandler(byte messageType, byte msg[], byte msgSize) {
           lcd.print(" KM");
         } else {
           lcd.print("PA: ");
-          lcd.print(periapsis_str);
+          lcd.print(periapsis_str);a
           lcd.print(" M");
         }
       }
