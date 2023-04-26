8953950b7e2c4bda59e92c3429ef7f6a5a2f925b
Fyryte <131640588+Fyryte@users.noreply.github.com>
2023-04-25 21:25:16 -0600

    Update KSPC_Experimental.ino

--- KSPC_Experimental.ino
+++ KSPC_Experimental.ino
@@ -1,46 +1,5 @@
-#include <LiquidCrystal_I2C.h>        // LCD Display
-#include <KerbalSimpit.h>             // SimPit     
-#include <KerbalSimpitMessageTypes.h> // SimPit
-#include <PayloadStructs.h>           // SimPit
-
-KerbalSimpit mySimpit(Serial);
-LiquidCrystal_I2C lcd(0x3F,16,2);
-
-char apoapsis_str[150];  // telemetry strings
-char periapsis_str[150];
-
-void setup() {
-    while (!mySimpit.init()) {         
-    delay(100);                         // Handshake protocol attempts ever 100ms until connection   
-     Serial.begin(115200);              // Initializes the serial communication
-  lcd.init();                           // LCD Display for the rest until the end of bracket
-  lcd.backlight();
-  lcd.print("   Working");
-  delay(500);
-  lcd.print(".");
-  delay(500);
-  lcd.print(".");
-  delay(500);
-  lcd.print(".");
-  delay(500);
-
-
-  }
-  lcd.clear();                          // Once handshake has been completed on game launch this block prints connected to SimPit in game and the LCD
-  lcd.print("   Connected!");
-  mySimpit.printToKSP("Connected", PRINT_TO_SCREEN);
-
-  mySimpit.registerChannel(APSIDES_MESSAGE); // register input channel
-  mySimpit.inboundHandler(messageHandler);   // register message handler
-}
-
-  void loop() {
-    lcd.backlight();
-    mySimpit.update(); //processes incoming messages
-}
-
 void messageHandler(byte messageType, byte msg[], byte msgSize) {
-  switch(messageType) {
+  switch (messageType) {
     case APSIDES_MESSAGE:
       if (msgSize == sizeof(apsidesMessage)) {
         apsidesMessage myApsides;
@@ -49,34 +8,31 @@ void messageHandler(byte messageType, byte msg[], byte msgSize) {
         dtostrf(myApsides.periapsis, 8, 0, periapsis_str);
         lcd.clear(); // clear the LCD screen
         lcd.setCursor(0, 0);
-        lcd.print("AP: ");
 
-        if (atoi(apoapsis_str) >= 1000) {
+        if (atol(apoapsis_str) >= 1000 || atol(apoapsis_str) <= -1000) {
           float apoapsis = atof(apoapsis_str) / 1000.0;
           char apoapsis_buf[8];
           dtostrf(round(apoapsis * 100) / 100.0, 6, 2, apoapsis_buf);
+          lcd.print("AP: ");
           lcd.print(apoapsis_buf);
           lcd.print(" KM");
-        } 
-        else {
+        } else {
+          lcd.print("AP: ");
           lcd.print(apoapsis_str);
           lcd.print(" M");
         }
 
         lcd.setCursor(0, 1);
-        lcd.print("PA: ");
 
-        if (atoi(periapsis_str) >= 1000) {
+        if (atol(periapsis_str) >= 1000 || atol(periapsis_str) <= -1000) {
           float periapsis = atof(periapsis_str) / 1000.0;
           char periapsis_buf[8];
           dtostrf(round(periapsis * 100) / 100.0, 6, 2, periapsis_buf);
+          lcd.print("PA: ");
           lcd.print(periapsis_buf);
           lcd.print(" KM");
-        } 
-        else if (atoi(periapsis_str) < 1) {
-          lcd.print("0.00 M");
-        } 
-        else {
+        } else {
+          lcd.print("PA: ");
           lcd.print(periapsis_str);
           lcd.print(" M");
         }
