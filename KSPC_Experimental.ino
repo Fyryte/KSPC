5266e09d17728a98c9abcb3efbe677008179eae3
Fyryte <131640588+Fyryte@users.noreply.github.com>
2023-04-26 20:13:29 -0600

    Update KSPC_Experimental.ino

--- KSPC_Experimental.ino
+++ KSPC_Experimental.ino
@@ -3,18 +3,15 @@
 #include <KerbalSimpitMessageTypes.h> // SimPit
 #include <PayloadStructs.h>           // SimPit
 
-
 KerbalSimpit mySimpit(Serial);
-LiquidCrystal_I2C lcd(0x3F,16,2);
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
 
 char apoapsis_str[150];  // telemetry strings
 char periapsis_str[150];
 
 void setup() {
-    while (!mySimpit.init()) {         
-    delay(100);                         // Handshake protocol attempts ever 100ms until connection   
-     Serial.begin(115200);              // Initializes the serial communication
-  lcd.init();                           // LCD Display for the rest until the end of bracket
+  Serial.begin(115200);  // Initializes the serial communication
+  lcd.init();             // LCD Display initialization
   lcd.backlight();
   lcd.print("   Working");
   delay(500);
@@ -25,59 +22,48 @@ void setup() {
   lcd.print(".");
   delay(500);
 
-
+  while (!mySimpit.init()) {
+    delay(100);  // Handshake protocol attempts every 100ms until connection
   }
-  lcd.clear();                          // Once handshake has been completed on game launch this block prints connected to SimPit in game and the LCD
+  
+  lcd.clear();  // Once handshake has been completed on game launch this block prints connected to SimPit in game and the LCD
   lcd.print("   Connected!");
   mySimpit.printToKSP("Connected", PRINT_TO_SCREEN);
-
-  mySimpit.registerChannel(APSIDES_MESSAGE); // register input channel
-  mySimpit.inboundHandler(messageHandler);   // register message handler
+  mySimpit.registerChannel(APSIDES_MESSAGE);  // register input channel
+  mySimpit.inboundHandler(messageHandler);    // register message handler
 }
 
-  void loop() {
-    lcd.backlight();
-    mySimpit.update(); //processes incoming messages
+void loop() {
+  mySimpit.update();  // processes incoming messages
 }
 
+// LCD MESSAGE HANDLER
 void messageHandler(byte messageType, byte msg[], byte msgSize) {
   switch (messageType) {
     case APSIDES_MESSAGE:
       if (msgSize == sizeof(apsidesMessage)) {
+        // AP and PA derived from APSIDES
         apsidesMessage myApsides;
         myApsides = parseApsides(msg);
-        dtostrf(myApsides.apoapsis, 8, 0, apoapsis_str);
-        dtostrf(myApsides.periapsis, 8, 0, periapsis_str);
-        lcd.clear(); // clear the LCD screen
+        
+        // Convert apoapsis and periapsis values to floats
+        float apoapsis = myApsides.apoapsis / 1000.0;
+        float periapsis = myApsides.periapsis / 1000.0;
+        
+        // Format apoapsis and periapsis values as strings
+        dtostrf(apoapsis, 6, 2, apoapsis_str);
+        dtostrf(periapsis, 6, 2, periapsis_str);
+        
+        // Display AP and PA on LCD
+        lcd.clear();  // clear the LCD screen
         lcd.setCursor(0, 0);
-
-        if (atol(apoapsis_str) >= 1000 || atol(apoapsis_str) <= -1000) {
-          float apoapsis = atof(apoapsis_str) / 1000.0;
-          char apoapsis_buf[8];
-          dtostrf(round(apoapsis * 100) / 100.0, 6, 2, apoapsis_buf);
-          lcd.print("AP: ");
-          lcd.print(apoapsis_buf);
-          lcd.print(" KM");
-        } else {
-          lcd.print("AP: ");
-          lcd.print(apoapsis_str);
-          lcd.print(" M");
-        }
-
+        lcd.print("AP: ");
+        lcd.print(apoapsis_str);
+        lcd.print(" KM");
         lcd.setCursor(0, 1);
-
-        if (atol(periapsis_str) >= 1000 || atol(periapsis_str) <= -1000) {
-          float periapsis = atof(periapsis_str) / 1000.0;
-          char periapsis_buf[8];
-          dtostrf(round(periapsis * 100) / 100.0, 6, 2, periapsis_buf);
-          lcd.print("PA: ");
-          lcd.print(periapsis_buf);
-          lcd.print(" KM");
-        } else {
-          lcd.print("PA: ");
-          lcd.print(periapsis_str);a
-          lcd.print(" M");
-        }
+        lcd.print("PA: ");
+        lcd.print(periapsis_str);
+        lcd.print(" KM");
       }
       break;
   }
