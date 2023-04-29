4188889e641edd054912fbb639210e5840cd4ac6
Fyryte <131640588+Fyryte@users.noreply.github.com>
2023-04-29 11:12:43 -0600

    Update KSPC.ino

--- KSPC.ino
+++ KSPC.ino
@@ -6,6 +6,23 @@
 KerbalSimpit mySimpit(Serial);
 LiquidCrystal_I2C lcd(0x3F,16,2);
 const int THROTTLE_PIN = A0; 
+// constants won't change. They're used here to
+// set pin numbers:
+const int buttonPin = 2;    // the number of the pushbutton pin
+const int ledPin = 13;      // the number of the LED pin
+
+// Variables will change:
+int ledState = HIGH;         // the current state of the output pin
+int buttonState;             // the current reading from the input pin
+int lastButtonState = LOW;   // the previous reading from the input pin
+
+// the following variables are unsigned long's because the time, measured
+// in miliseconds, will quickly become a bigger number than can be stored
+// in an int.
+unsigned long lastDebounceTime = 0;  // the last time the output pin
+                                     // was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase
+                                     // if the output flickers
 
 
 void setup() {
@@ -46,6 +63,44 @@ void setup() {
 
 //processes incoming messages
 void loop() {
+    // Read the state of the switch into a local variable.
+  int reading = digitalRead(buttonPin);
+
+  // check to see if you just pressed the button
+  // (i.e. the input went from LOW to HIGH),  and you've waited
+  // long enough since the last press to ignore any noise:
+
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer
+    // than the debounce delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      // If the new button state is HIGH, that means the button
+      // has just been pressed.
+      if (buttonState == HIGH) {
+        // Send a message to the plugin activating the Stage
+        // action group. The plugin will then activate the
+        // next stage.
+        mySimpit.activateAction(STAGE_ACTION);
+      }
+    }
+  }
+
+  // Set the LED to match the state of the button.
+  digitalWrite(ledPin, buttonState);
+
+  // save the reading.  Next time through the loop,
+  // it'll be the lastButtonState:
+  lastButtonState = reading;
   throttleMessage throttle_msg;
   // read the input on analog pin 0:
   int reading = analogRead(THROTTLE_PIN);
